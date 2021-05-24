ff0a0dc2bbf93096cd36c8d85a4f6214189dcca0
Athira-999 <62925803+Athira-999@users.noreply.github.com>
2021-05-24 10:21:45 +0530

    Update untestcase.py

--- website/seleniumtest/untestcase.py
+++ website/seleniumtest/untestcase.py
@@ -12,18 +12,22 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 class TestPhpWebsite(unittest.TestCase):
     def test_php(self):
         dockerstop = "docker stop phpcontainer"
+        dockerremove = "docker rm phpcontainer"
         actual_text = self.seltest()
+        print("Actual text is:")
         print(actual_text)
         print('\n')
         readFile = open("phpapp.txt", 'r')
         expected_text = readFile.read()
         readFile.close()
+        print("Expected text is:")
         print(expected_text)
         if (expected_text != actual_text):
             subprocess.call(dockerstop, shell=True)
+            subprocess.call(dockerremove, shell=True)
         assert True
-        self.assertEqual(actual_text,expected_text,msg="Actual and expected does not match")
-        print("Matching")
+        self.assertEqual(actual_text,expected_text,msg="Actual and expected text does not match, hence stopping and removing container from Test Server")
+        print("Actual and Expected text is Matching. Testing is success. Hence deploying to Prod Server")
 
     def seltest(self):
         driver = webdriver.Chrome(executable_path='/usr/lib64/chromium-browser/chromedriver',chrome_options=chrome_options)
